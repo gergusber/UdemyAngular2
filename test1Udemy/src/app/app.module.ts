d3479c229a428a0deea6abaf21fcdddc9f82e8aa
german.bertea@globant.com <JU934AZ@AR1255496W1>
2019-08-28 21:15:34 -0300

    first test complete

--- test1Udemy/src/app/app.module.ts
+++ test1Udemy/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { WarningalertComponent } from './warningalert/warningalert.component';
 import { SuccessalertComponent } from './successalert/successalert.component';
 
@@ -15,7 +15,7 @@ import { SuccessalertComponent } from './successalert/successalert.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
