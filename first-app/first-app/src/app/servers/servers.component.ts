9f50c012d0d22c81390d5800bb7ecfef7a6f53d5
germanbertea <german.bertea@globant.com>
2019-10-21 16:37:14 -0300

    hasta el video 31

--- first-app/first-app/src/app/servers/servers.component.ts
+++ first-app/first-app/src/app/servers/servers.component.ts
@@ -6,10 +6,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName='Test';
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  };
+  
+  onCreateServer(){
+    this.serverCreationStatus ='server was created!';
+  };
+  onUpdateServerName(event: any){
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
-
 }
